2774ed750ffb4aefab4e7eab310f1240184e948e
leckman1234 <haidarvitalii@gmail.com>
2023-03-07 19:43:30 +0200

    added info

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ class Student:
     def info(self):
         name = self.name
         species = self.species
-        hungary = self.hungry
+        hungry = self.hungry
         tired = self.tired
         happy = self.happy
-    if self.hungry:
-        print(f"{self.name} has been fed.")
-        self.hungry = False
-    else:
-        print(f"{self.name}"is not hungary)
+
+        if self.hungry:
+            print(f"{self.name} has been fed.")
+            self.hungry = False
+        else:
+            print(f"{self.name} is not hungry.")
+    def info(self):
+        if self.tired:
+            print(f"{self.name} has gone to sleep.")
+            self.tired = False
+        else:
+            print(f"{self.name} is not tired.")
+    def info(self):
+        if self.happy:
+            print(f"{self.name} is already playing.")
+        else:
+            print(f"{self.name} is playing now.")
+            self.happy = True
+
+
 
