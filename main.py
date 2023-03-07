8e946f39351321e5194846ef42b1dfa6e8311a9c
leckman1234 <haidarvitalii@gmail.com>
2023-03-07 19:37:55 +0200

    added class

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-class student:
-    def__init__(self,)
+class Student:
+    def __init__(self,name,species,hungry,tired,happy):
+        self.name = name
+        self.species = species
+        self.hungry = True
+        self.tired = True
+        self.happy = False
+    def info(self):
+        name = self.name
+        species = self.species
+        hungary = self.hungry
+        tired = self.tired
+        happy = self.happy
+    if self.hungry:
+        print(f"{self.name} has been fed.")
+        self.hungry = False
+    else:
+        print(f"{self.name}"is not hungary)
+
